d8e429defa21b6aa289ae496a86757028072f5d8
Cheruiyot Robert <kiruir38@gmail.com>
2024-04-13 15:24:24 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42); // Assuming Scuber's headquarters is at block 42
+  }
+
+  function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264; // Each block is 264 feet long
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    const distance = Math.abs(destination - start) * 264; // Each block is 264 feet long
+    return distance;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    let fare = 0;
+    
+    if (distance <= 400) {
+      fare = 0; // First 400 feet are free
+    } else if (distance <= 2000) {
+      fare = (distance - 400) * 0.02; // 2 cents per foot after the first 400 feet
+    } else if (distance <= 2500) {
+      fare = 25; // Flat fare of $25 for distance over 2000 feet and under 2500 feet
+    } else {
+      return 'cannot travel that far'; // Scuber does not allow rides over 2500 feet
+    }
+    
+    return fare;
+  }
+  
